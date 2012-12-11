42997b460f0218335d3090b03ed35c949b9ca58e
tcz <git@tcz.hu>
2012-12-11 18:48:57 +0100

    Emitting PageRank from this site to each outlink

--- map.php
+++ map.php
@@ -13,4 +13,11 @@ while ( true )
 
     list( $url, $page_rank, $links )    = preg_split( "/\s+/", $line, 3, PREG_SPLIT_NO_EMPTY );
     $links                              = preg_split( "/\s+/", $links, -1, PREG_SPLIT_NO_EMPTY );
+
+    foreach ( $links as $link )
+    {
+        echo "$link\t";
+        echo "N" . $page_rank / count( $links );
+        echo "\n";
+    }
 }
