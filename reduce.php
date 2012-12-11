68e338c4f83c04fb3f78914d129c047ca4795063
tcz <git@tcz.hu>
2012-12-11 18:53:43 +0100

    Parsing input to key and value

--- reduce.php
+++ reduce.php
@@ -6,6 +6,8 @@ while ( true )
     $line = fgets( STDIN );
     $line = rtrim( $line, "\n\r" );
 
+    list( $key, $value ) = preg_split( "/\t/", $line, 2, PREG_SPLIT_NO_EMPTY );
+
     if ( empty( $line ) )
     {
         break;
