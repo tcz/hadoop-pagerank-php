43bbd295c8866cf550d08fcff590ed501340859f
tcz <git@tcz.hu>
2012-12-11 18:55:22 +0100

    Tracking a few variables between iterations

--- reduce.php
+++ reduce.php
@@ -1,6 +1,10 @@
 #!/usr/bin/php
 <?php
 
+$last_key = null;
+$links = array();
+$new_page_rank = 0;
+
 while ( true )
 {
     $line = fgets( STDIN );
@@ -12,4 +16,6 @@ while ( true )
     {
         break;
     }
+
+    $last_key = $key;
 }
