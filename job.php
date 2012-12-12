2046ce095cd02a924a16c73234101d568c200212
tcz <git@tcz.hu>
2012-12-12 16:46:38 +0100

    We will use the functions created earlier

--- job.php
+++ job.php
@@ -5,3 +5,7 @@ if ( $argc !== 3 )
 {
     exit( "Usage: " . basename( __FILE__ ) . " [input_path] [output_path_prefix]\n\n" );
 }
+
+require_once __DIR__ . '/lib/deleteFile.php';
+require_once __DIR__ . '/lib/runJob.php';
+require_once __DIR__ . '/lib/readCounter.php';
